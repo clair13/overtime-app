ad15eec4b6b3035baabb5bceb846eeb48c468fd2
clair13 <theclairvoyant1313@yahoo.gr>
2016-10-09 15:21:20 +0200

    Built out rake task for sending messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                                         password: "asdfasdf",
                                         password_confirmation: "asdfasdf",
                                         first_name: "Jon",
                                         last_name: "Snow",
                                         phone: "4322386131")
 
-puts "1 User created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com",
                                     password: "asdfasdf",
@@ -14,17 +14,16 @@ AdminUser.create(email: "admin@test.com",
                                     last_name: "Name",
                                     phone: "4322386131")
 
-puts "1 Admin User created"
+puts "1 Admin user created"
+
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
 
 100.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content Kinfolk gentrify you probably haven't heard of them try-hard, swag lo-fi lyft biodiesel cold-pressed. Everyday carry tattooed listicle blue bottle, sartorial post-ironic hot chicken lomo cornhole shoreditch DIY. Put a bird on it neutra taxidermy tumblr. Drinking vinegar literally humblebrag, edison bulb XOXO celiac biodiesel pour-over beard. Irony sartorial biodiesel 8-bit, PBR&B poutine small batch flexitarian +1 pitchfork tote bag mumblecore. Neutra marfa lo-fi, iceland jianbing hashtag venmo vape vinyl poke polaroid taxidermy.", user_id: @user.id, overtime_request: 2.5)
+    Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
-
-
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 audit logs have been created"
